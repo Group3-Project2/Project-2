f38c63584c1e7c0704ac1bd03956ac87788afb92
Kayle7777 <jessewebb2@gmail.com>
2018-09-13 19:36:46 -0500

    Integrated all of Emily's work, only added a few html routes, small amount of jquery to work with the navbar redirects.

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-
+const bodyParser = require('body-parser');
 const express = require("express");
 const db = require("./models");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+
+// require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
