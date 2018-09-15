daf154a0e34cfea9f8267b580add41f11e6175fa
Daniel Rodriguez <drodrigu35@gmail.com>
2018-09-15 11:30:01 -0500

    what

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require("express");
+process.env.NODE_ENV = ""
 const db = require("./models");
 const app = express();
 const PORT = process.env.PORT || 8080;
